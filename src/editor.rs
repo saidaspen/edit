8d8d684ec6825684bb473a7ddd72f113bd47da8b
Said Aspen <mail@saidaspen.se>
2021-01-20 21:36:24 +0100

    Almost through chapter 4

--- src/editor.rs
+++ src/editor.rs
@@ -1,8 +1,12 @@
+use crate::Document;
 use crate::Terminal;
+use crate::Row;
+use std::env;
 use termion::event::Key;
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
+#[derive(Default)]
 pub struct Position {
     pub x: usize,
     pub y: usize,
@@ -11,7 +15,9 @@ pub struct Position {
 pub struct Editor {
     shoud_quit: bool,
     terminal: Terminal,
+    document: Document,
     cursor_position: Position,
+    offset: Position,
 }
 
 fn die(e: std::io::Error) {
@@ -35,22 +41,35 @@ impl Editor {
     }
 
     pub fn default() -> Self {
+        let args: Vec<String> = env::args().collect();
+        let document = if args.len() > 1 {
+            let file_name = &args[1];
+            Document::open(&file_name).unwrap_or_default()
+        } else {
+            Document::default()
+        };
+
         Self {
             shoud_quit: false,
             terminal: Terminal::default().expect("Failed to initialize terminal"),
-            cursor_position: Position { x: 0, y: 0 },
+            document: document,
+            cursor_position: Position::default(),
+            offset: Position::default(),
         }
     }
 
     fn refresh_screen(&self) -> Result<(), std::io::Error> {
         Terminal::cursor_hide();
-        Terminal::cursor_position(&Position { x: 0, y: 0 });
+        Terminal::cursor_position(&Position::default()); 
         if self.shoud_quit {
             Terminal::clear_screen();
             println!("Goodbye.\r");
         } else {
             self.draw_rows();
-            Terminal::cursor_position(&self.cursor_position);
+            Terminal::cursor_position(&Position{
+                x : self.cursor_position.x.saturating_add(self.offset.x),
+                y : self.cursor_position.y.saturating_add(self.offset.y),
+            });
         }
         Terminal::cursor_show();
         Terminal::flush()
@@ -70,13 +89,34 @@ impl Editor {
             | Key::Home => self.move_cursor(pressed_key),
             _ => (),
         }
+        self.scroll();
         Ok(())
+    }
+    fn scroll(&mut self) {
+        let Position{x, y } = self.cursor_position;
+        let width = self.terminal.size().width as usize;
+        let height = self.terminal.size().height as usize;
+        let mut offset = &mut self.offset;
+        if y < offset.y {
+            offset.y = y
+        } else if y >= offset.y.saturating_add(height) {
+            offset.y = y.saturating_add(height).saturating_add(1);
+        }
+        if x < offset.x {
+            offset.x = x;
+        } else if x >= offset.x.saturating_add(width) {
+            offset.x = offset.x.saturating_add(width) + 1;
+        }
     }
     fn move_cursor(&mut self, key: Key) {
         let Position { mut x, mut y } = self.cursor_position;
         let size = self.terminal.size();
-        let height = size.height.saturating_sub(1) as usize;
-        let width = size.width.saturating_sub(1) as usize;
+        let height = self.document.len();
+        let mut width = if let Some(row) = self.document.row(y){
+            row.len()
+        } else {
+            0
+        };
         match key {
             Key::Up => y = y.saturating_sub(1),
             Key::Down => {
@@ -96,13 +136,30 @@ impl Editor {
             Key::End => x = width,
             _ => (),
         }
+        width = if let Some(row) = self.document.row(y) {
+            row .len()
+        } else {
+            0 
+        };
+        if x > width {
+            x = width;
+        }
         self.cursor_position = Position { x, y }
+    }
+    fn draw_row(&self, row: &Row) {
+        let width = self.terminal.size().width as usize;
+        let start = self.offset.x; 
+        let end = self.terminal.size().width as usize + width;
+        let row = row.render(start, end);
+        println!("{}\r", row)
     }
     fn draw_rows(&self) {
         let height = self.terminal.size().height;
-        for row in 0..height - 1 {
+        for terminal_row in 0..height - 1 {
             Terminal::clear_current_line();
-            if row == height / 3 {
+            if let Some(row) = self.document.row(terminal_row as usize + self.offset.y) {
+                self.draw_row(row);
+            } else if self.document.is_empty() && terminal_row == height / 3 {
                 self.draw_welcome_message();
             } else {
                 println!("~\r");
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 #![warn(clippy::all, clippy::pedantic)]
 mod editor;
-
+mod document;
 mod terminal;
+mod row;
 
 use editor::Editor;
 use editor::Position;
 pub use terminal::Terminal;
+pub use document::Document;
+pub use row::Row;
 
 fn main() {
     Editor::default().run();
